23b5a820bc30b654fc585dcd28976c4d0e385e6d
Ophelie Bellissens <obellissens1@gmail.com>
2023-02-02 11:53:45 +0100

    Ajout Html CSS Login

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,41 +1,10 @@
-// Création des buttons 
-
-const fetchPromiseCat = fetch('http://localhost:5678/api/categories')
-  .then(reponse => reponse.json())
-  .then((data) => {
-    categories=data;
-    genererButton(categories);
-    console.log(categories);
-  })
-  .catch(error => {
-    console.error(`Impossible de recuperer les categories: ${error}`)
-    })
-  
-function genererButton(categories){
-    const divButton = document.querySelector(".buttons");
-    const Tous= new Set(categories);
-    const buttonTous = document.createElement('button');
-    buttonTous.innerText="Tous";
-    console.log(Tous);
-    divButton.appendChild(buttonTous)
-  for (let i=0; i<categories.length;i++){
-    const article = categories[i];
-    const button= document.createElement('button');
-    button.innerText = article.name;
-    divButton.appendChild(button)
-  }
-}
-//Filtres
-
-
-
 //Création de la galerie
 const fetchPromise = fetch('http://localhost:5678/api/works')
       .then(reponse => reponse.json())
       .then((data) => {
         projet=data;
         genererWorks(projet);//Mettre la fonction dans le then pour que ça marche
-        console.log(projet);
+        console.table(projet);
       })
       .catch(error => {
         console.error(`Impossible de récupérer les produits : ${error}`);
@@ -53,6 +22,33 @@ function genererWorks(projet){
         titleElement.innerText = article.title;
         sectionGallery.appendChild(figureElement);
         figureElement.appendChild(imgElement);
-        figureElement.appendChild(titleElement)
-}}
+        figureElement.appendChild(titleElement);
+      }
+    }
 
+// Création des buttons 
+const fetchPromiseCat = fetch('http://localhost:5678/api/categories')
+  .then(reponse => reponse.json())
+  .then((data) => {
+    categories=data;
+    genererButton(categories);
+    console.table(categories);
+  })
+  .catch(error => {
+    console.error(`Impossible de recuperer les categories: ${error}`)
+    })
+  
+function genererButton(categories){
+    const divButton = document.querySelector(".buttons");
+    const Tous= new Set(categories);
+    const buttonTous = document.createElement('button');
+    buttonTous.innerText="Tous";
+    console.log(Tous);
+    divButton.appendChild(buttonTous)
+  for (let i=0; i<categories.length;i++){
+    const article = categories[i];
+    const button= document.createElement('button');
+    button.innerText = article.name;
+    divButton.appendChild(button);    
+    }
+  }
