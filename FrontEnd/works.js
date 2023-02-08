eaf00996ef1fbb4e412e29d4526f5c18fa5753d9
Ophelie Bellissens <obellissens1@gmail.com>
2023-02-08 18:24:45 +0100

    Ajout des filtres

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,54 +1,85 @@
-//Création de la galerie
-const fetchPromise = fetch('http://localhost:5678/api/works')
-      .then(reponse => reponse.json())
-      .then((data) => {
-        projet=data;
-        genererWorks(projet);//Mettre la fonction dans le then pour que ça marche
-        console.table(projet);
-      })
-      .catch(error => {
-        console.error(`Impossible de récupérer les produits : ${error}`);
-      });
-      
-function genererWorks(projet){
-        for (let i=0; i<projet.length; i++){
-        const sectionGallery = document.querySelector('.gallery');
-        const article = projet[i]
-        const figureElement = document.createElement('figure');
-        const imgElement = document.createElement('img');
-        imgElement.src = article.imageUrl;
-        imgElement.crossOrigin = "anonymous";
-        const titleElement = document.createElement('figcaption');
-        titleElement.innerText = article.title;
-        sectionGallery.appendChild(figureElement);
-        figureElement.appendChild(imgElement);
-        figureElement.appendChild(titleElement);
+const reponse = await fetch("http://localhost:5678/api/works");
+const works = await reponse.json();
+
+//génération de la galerie
+    function genererWork () {
+          for (let i = 0; i < works.length; i++) {
+            //Selection de l'emplacement dans le html
+            const sectionGallery = document.querySelector(".gallery");
+            const article = works[i];
+            //Création de la figure
+            let figureElement = document.createElement("figure");
+            figureElement.setAttribute("data-category-id", works[i].category.id)
+            figureElement.classList.add('projet');
+            //Création de l'image
+            let imgElement = document.createElement("img");
+            imgElement.src = article.imageUrl;
+            imgElement.crossOrigin = "anonymous";
+            //Création du figcaption
+            let titleElement = document.createElement("figcaption");
+            titleElement.innerText = article.title;
+            //Insertion des enfants dans les parents
+            sectionGallery.appendChild(figureElement);
+            figureElement.appendChild(imgElement);
+            figureElement.appendChild(titleElement);
       }
     }
+//appel à la fonction pour générer la galerie
+genererWork(works);
 
-// Création des buttons 
-const fetchPromiseCat = fetch('http://localhost:5678/api/categories')
-  .then(reponse => reponse.json())
-  .then((data) => {
-    categories=data;
-    genererButton(categories);
-    console.table(categories);
-  })
-  .catch(error => {
-    console.error(`Impossible de recuperer les categories: ${error}`)
-    })
-  
-function genererButton(categories){
-    const divButton = document.querySelector(".buttons");
-    const Tous= new Set(categories);
-    const buttonTous = document.createElement('button');
-    buttonTous.innerText="Tous";
-    console.log(Tous);
-    divButton.appendChild(buttonTous)
-  for (let i=0; i<categories.length;i++){
-    const article = categories[i];
-    const button= document.createElement('button');
-    button.innerText = article.name;
-    divButton.appendChild(button);    
+
+//génération du tableau de Catégories uniques
+    function generateUniqueCategories(){
+      let categoriesUniques = [];
+      for ( let work of works){
+        const result = categoriesUniques.find(function(category){
+          return category.id === work.category.id;
+        });
+        if(result === undefined){
+          categoriesUniques.push(work.category);
+        }
+        
+      }
+      return categoriesUniques;
     }
+
+
+//génération des filtres
+const categories = generateUniqueCategories();
+const filterContainerTous = document.querySelector('.filter-btn');
+//Génération du filtre Tous
+let allCategoriesFilter = document.createElement('button');
+allCategoriesFilter.innerText = 'Tous';
+filterContainerTous.appendChild(allCategoriesFilter);
+//Ajout de l'évent click affichant toutes les catégories
+allCategoriesFilter.addEventListener('click', function() {
+  const figures = document.querySelectorAll('figure');
+  for (let figure of figures) {
+    //Affiche tout les block catégories
+    figure.style.display = 'block';
   }
+});
+//Génération des 3 filtres 
+const filterContainer = document.querySelector('.filter-btn');
+for (let category of categories) {
+
+  let filterButton = document.createElement('button');
+  filterButton.innerText = category.name;
+  filterButton.setAttribute('data-category-id', category.id);
+  filterButton.addEventListener('click', function() {
+    const figures = document.querySelectorAll('.projet');
+    for (let figure of figures) {
+      //Affiche les éléments ayant la catégorie choisie, sinon les faits disparaitres
+      if (figure.getAttribute('data-category-id') == category.id) {
+        figure.style.display = 'block';
+      } else {
+        figure.style.display = 'none';
+      }
+    }
+  });
+  filterContainer.appendChild(filterButton);
+}
+
+
+
+
